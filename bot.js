00921e48c5a70f6b7ed723d36548b103c97beed6
omar0700bot <59139063+omar0700bot@users.noreply.github.com>
2020-10-12 13:54:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "!";
+var prefix = "8";
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Avatar ..`,"http://twitch.tv/EE")
+client.user.setGame(`ready? join`,"http://twitch.tv/EE")
 client.user.setStatus("dnd")
 });
 
